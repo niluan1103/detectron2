671e0fd75f89b49f1348ba0550d99693739240d9
niluan1103 <80779231+niluan1103@users.noreply.github.com>
2022-12-09 20:24:26 +0700

    Update st.py

--- st.py
+++ st.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
 
         st.text('Inferencing')
         img = np.array(img)
-        visualized_img = wrist_detect.for_streamlit(img)
-        st.image(visualized_img,'Visualized prediction')
+        #visualized_img = wrist_detect.for_streamlit(img)
+        #st.image(visualized_img,'Visualized prediction')
