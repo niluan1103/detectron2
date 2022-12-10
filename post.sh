fab47b800c351269783ee207e998466a3d48b89e
niluan1103 <80779231+niluan1103@users.noreply.github.com>
2022-12-10 10:16:43 +0700

    Create post.sh

---
+++ post.sh
@@ -0,0 +1,9 @@
+apt-get install gcc
+apt install git-all
+apt install build-essential
+pip install --upgrade pip
+pip install --upgrade setuptools wheel
+pip install cython
+pip install ninja
+git clone https://github.com/facebookresearch/detectron2.git
+python -m pip install -e detectron2
